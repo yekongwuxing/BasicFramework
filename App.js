a86700c23ff4d390a502244a7fdfe22960edd726
jiangmm <jiangmm@jchvip.com>
2018-08-24 14:10:36 +0800

    字体不随系统变化而改变

--- App.js
+++ App.js
@@ -7,11 +7,20 @@
  */
 
 import React, {Component} from 'react';
+import {Text} from 'react-native';
 import AppNavigator from './src/Index';
 import './src/untils/Theme';
 
 type Props = {};
 export default class App extends Component<Props> {
+  // 构造
+    constructor(props) {
+      super(props);
+      // 初始状态
+      this.state = {};
+      //属性可以控制不随系统字体缩放
+      Text.defaultProps.allowFontScaling = false;
+    }
   render() {
     return (
       <AppNavigator />
